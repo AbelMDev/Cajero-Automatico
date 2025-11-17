706080a05d120bf6d25fd4a57250f859dd53918c
AbelMDev <abel.murgas@uip.edu.pa>
2025-11-17 18:03:55 -0500

    Crear archivo base

---
+++ main.py
@@ -0,0 +1,10 @@
+saldo = 1000  # saldo inicial
+opcion = 0
+print("💰 Bienvenido a tu Cajero Automático")
+while opcion != 4:
+    print("\n--- MENÚ ---")
+    print("1. Consultar saldo")
+    print("2. Depositar dinero")
+    print("3. Retirar dinero")
+    print("4. Salir")
+    opcion = int(input("Selecciona una opción: "))
